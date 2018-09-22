3305d4bad5d14a2b443824f4a6ee840b74db27a2
mynameiskate <katerina.akent@gmail.com>
2018-09-22 18:17:17 +0300

    First task added

---
+++ JavaIntro/src/tests/CalcTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+import tasks.CalcTask;
+import static org.junit.Assert.assertEquals;
+
+public class CalcTest {
+
+    @Test
+    public void testCompute() {
+        CalcTask task = new CalcTask();
+        assertEquals(1.5, task.calculate(1,-1),0.001);
+    }
+}
